357a727c5d225cdf2509776ec1fd1aca3c9afc26
chehir <abe.crunck@gmail.com>
2017-06-11 02:59:48 +0100

    login static

--- src/AppBundle/Controller/TodoController.php
+++ src/AppBundle/Controller/TodoController.php
@@ -9,6 +9,7 @@
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Todo;
+use AppBundle\Entity\User;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
@@ -17,6 +18,8 @@ use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 
 class TodoController extends Controller {
@@ -118,16 +121,65 @@ class TodoController extends Controller {
 	}
 
 	/**
-	 * @Route("/todos/delete/{id}", name="todo_details")
+	 * @Route("/todos/delete/{id}", name="todo_delete")
 	 */
 	public function deleteAction($id, Request $request)
 	{
 		$todo = $this->getDoctrine()->getRepository('AppBundle:Todo')->find($id);
 		$em = $this->getDoctrine()->getManager();
 		$em->remove($todo);
-		//$em->flush();
+		$em->flush();
 		$this->addFlash('notice','Todo deleted');
 		return $this->redirectToRoute('todo_list');
 	}
 
+
+	/**
+	 * @Route("/testuser", name="just_fake_user_add")
+	 */
+	public function registerAction(UserPasswordEncoderInterface $encoder)
+	{
+		// whatever *your* User object is
+		$user = new User();
+		$user->setUsername('test');
+		$user->setEmail('test@test.com');
+		$plainPassword = 'test';
+		$encoded = $encoder->encodePassword($user, $plainPassword);
+
+		$user->setPassword($encoded);
+		$em = $this->getDoctrine()->getManager();
+		$em->persist($user);
+		$em->flush();
+
+		return $this->redirectToRoute('todo_list');
+
+	}
+
+
+	/**
+	 * @Route("/login", name="login")
+	 */
+	public function loginAction(Request $request, AuthenticationUtils $authUtils)
+	{
+		// get the login error if there is one
+		$error = $authUtils->getLastAuthenticationError();
+
+		// last username entered by the user
+		$lastUsername = $authUtils->getLastUsername();
+
+		return $this->render('todo/login.html.twig', array(
+			'last_username' => $lastUsername,
+			'error'         => $error,
+		));
+	}
+
+
+	/**
+	 * @Route("/logout", name="logout")
+	 */
+	public function logoutAction(Request $request, AuthenticationUtils $authUtils)
+	{
+		// get the login error if there is one
+		return null;
+	}
 }
